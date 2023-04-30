f7c4dda4198a1c49672faacf420fe0ad48521b17
Min Thant <kthura397@gmail.com>
2023-05-01 00:40:40 +0630

    JSON stringfify

--- public/script.js
+++ public/script.js
@@ -13,6 +13,10 @@ const upload = async () => {
     const inputTag = document.getElementById("uploadFile");
     const files = [...inputTag.files];
     const formData = new FormData();
+    formData.append(
+        "user",
+        JSON.stringify({ name: "user", email: "user1@gmail.com", age: 30 })
+    );
     files.forEach((file) => formData.append("files", file));
     const response = await fetch(`${apiUrl}/uploadFile`, {
         method: "POST",
